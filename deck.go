a0edf36556abbdfb8231069240ff5eb029f874a4
albertanugerah <albertanguerah@github.com>
2021-01-06 21:57:44 +0700

    belajar go

---
+++ deck.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// create a new type of 'deck'
+// which is  a slice of strings
+type deck []string
+
+func newDeck() deck {
+
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// func printState di file state.go
+	printState()
+
+	cards := deck{"Ace of Diamonds", newCard()}
+	// menambahkan nilai card
+	cards = append(cards, "Six of Spades")
+	cards.print()
+}
+
+func newCard() string { // ketika di eksekusi, function mengembalikan tipe data
+	return "Five of Diamonds"
+}
+
+// func [newfunction]() [type_data]
